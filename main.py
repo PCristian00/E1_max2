ff74fd3cfbd3889415e47a2ec05193c3017c0ca4
PCristian00 <cristianp9300@gmail.com>
2022-04-04 19:27:05 +0200

    Soluzione temporanea con casting

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 def my_max(a, b):
-    print(' a '+str(a)+' b '+str(b))
+    print(' a ' + str(a) + ' b ' + str(b))
     if a == b:
         print('I numeri sono identici')
         print(str(a) + ' == ' + str(b))
     elif a > b:
         print('Il numero più grande tra i due è ' + str(a))
-        print(str(a)+' > '+str(b))
+        print(str(a) + ' > ' + str(b))
     else:
         print('Il numero più grande tra i due è ' + str(b))
         print(str(b) + ' > ' + str(a))
 
 
-x = input('Inserire primo numero: ')
-y = input('Inserire secondo numero: ')
+x = int(input('Inserire primo numero: '))
+y = int(input('Inserire secondo numero: '))
 my_max(x, y)
 print('Usando max')
-print(max(x,y))
+print(max(x, y))
